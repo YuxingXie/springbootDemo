9d2fc2a7d604a0dd3b75e1d26859d0e15d57ab59
xieyuxing <185246042@qq.com>
2017-12-06 17:17:23 +0800

    little change

---
+++ src/main/java/com/lingyuntec/test/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.lingyuntec.test;
+
+import com.lingyuntec.framework.config.Application;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by xieyuxing on 2017/12/1.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Application.class)
+public class ApplicationTest {
+}
